9a8304e60e45825ddd06ab35df28bc62cb4736da
Raed Al Sharif <92861612+RaedAlSharif@users.noreply.github.com>
2022-09-18 02:22:05 +0300

    fk all

--- lib/aks.dart
+++ lib/aks.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 List<String> list = <String>['سيارات   29', 'حريق   34', 'تأمينات هندسية   36', 'تأمينات عامه   35'
   ,'بحري   30','الطبي   1','اجسام طائرات   32','  اجسام سفن   31'];
@@ -17,6 +15,8 @@ class aks extends StatefulWidget {
 
 class _aksState extends State<aks> {
   BestTutorSite _site = BestTutorSite.javatpoint;
+  String str = "";
+  String str2 = "";
 
   String dropdownValue = list.first;
   final numController = TextEditingController();
@@ -27,125 +27,138 @@ class _aksState extends State<aks> {
   @override
   Widget build(BuildContext context) =>
 
-    Container(decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage("lib/img/manara.png"),
-            fit: BoxFit.cover
-        )
-    ),
-    child: Scaffold(
+      Container(decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("lib/img/manara.png"),
+              fit: BoxFit.cover
+          )
+      ),
+        child: Scaffold(
 
 
 
-    body: Column(
+          body: Column(
 
 
-    children: [
+            children: [
 
-    ListTile(
-    title: const Text('عقد'),
-    leading: Radio(
-    value: BestTutorSite.javatpoint,
-    groupValue: _site,
-    onChanged: (BestTutorSite? value) {
-    setState(() {
-    _site = value!;
-    });
-    },
-    ),
-    ),
-    ListTile(
-    title: const Text('ملحق'),
-    leading: Radio(
-    value: BestTutorSite.w3schools,
-    groupValue: _site,
-    onChanged: (BestTutorSite? value) {
-    setState(() {
-    _site = value!;
-    });
-    }
-    ),
-    ),
-
-
-    Center(
-    child: TextFormField(
-    controller: numController,
-    decoration: new InputDecoration(labelText: "رقم العقد/الملحق"),
-    keyboardType: TextInputType.number,
-    inputFormatters: <TextInputFormatter>[
-    FilteringTextInputFormatter.digitsOnly
-    ],
-    ),
-    ),
-    Center(
-    child: TextFormField(
-    controller: incontroller,
-    decoration: new InputDecoration(labelText: "المؤمن له"),
-    ),
-    ),
-
-    Center(
-    child: TextFormField(
-    controller: yearcontroller,
-    decoration: new InputDecoration(labelText: "السنة"),
-    keyboardType: TextInputType.number,
-    inputFormatters: <TextInputFormatter>[
-    FilteringTextInputFormatter.digitsOnly
-    ],
-    ),
-    ),
-    DropdownButton<String>(
-    value: dropdownValue,
-    icon: const Icon(Icons.arrow_downward),
-    elevation: 16,
-    style: const TextStyle(color: Colors.deepPurple),
-    underline: Container(
-    height: 2,
-    color: Colors.deepPurpleAccent,
-    ),
-    onChanged: (String? value) {
-    // This is called when the user selects an item.
-    setState(() {
-    dropdownValue = value!;
-    });
-    },
-    items: list.map<DropdownMenuItem<String>>((String value) {
-    return DropdownMenuItem<String>(
-    value: value,
-    child: Text(value),
-    );
-    }).toList(),
-    )
-    ,
-    TextButton(onPressed: () async {
-      final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
-      final response = await http.post(
-          url,
-        headers: {
-            'origin': 'http://localhost',
-            'Content-Type': 'application.json'
-        },
-        body: jsonEncode(
-          {
-            'service_id': "service_at95yok",
-            'template_id': "template_gg5sqzl",
-            'template_params': {
-              'user_subject':  " طلب "  + _site.toString(),
-              'user_message':"رقم العقد او الملحق : " + numController.text + "\n" +
-                  "السنه : " + yearcontroller.text + "\n" +
-                  "المؤمن له : " + incontroller.text + "\n" +
-                  "نوع التأمين : " + dropdownValue
-            }
-          },
-        )
-      );
-      print(response.body);
+              ListTile(
+                title: const Text('عقد'),
+                leading: Radio(
+                  value: BestTutorSite.javatpoint,
+                  groupValue: _site,
+                  onChanged: (BestTutorSite? value) {
+                    setState(() {
+                      _site = value!;
+                      str = "عقد";
+                      str2 = "عقد";
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('ملحق'),
+                leading: Radio(
+                    value: BestTutorSite.w3schools,
+                    groupValue: _site,
+                    onChanged: (BestTutorSite? value) {
+                      setState(() {
+                        _site = value!;
+                        str = "ملحق";
+                        str2 = "ملحق";
+                      });
+                    }
+                ),
+              ),
 
 
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: numController,
+                  decoration: new InputDecoration( border: OutlineInputBorder(),labelText:"رقم " + "ال" + str2),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: incontroller,
+
+                  decoration: new InputDecoration( border: OutlineInputBorder(),labelText: "المؤمن له"),
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: yearcontroller,
+                  decoration: new InputDecoration( border: OutlineInputBorder(),labelText: "السنة"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+              ),
+              DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              )
+              ,
 
 
-   /* final Email send_email = Email(
+              TextButton(onPressed: () async {
+
+                final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+                final response = await http.post(
+                    url,
+                    headers: {
+                      'origin': 'http://localhost',
+                      'Content-Type': 'application/json'
+                    },
+                    body: json.encode(
+                      {
+                        'service_id': 'service_at95yok',
+                        'template_id': 'template_gg5sqzl',
+                        'user_id' : 'Y__n685mzGVQ-WAz2',
+                        'template_params': {
+                          'user_subject':  " طلب عكس قيد "  + str ,
+
+                          'user_message1':"رقم ال" + str2 + " : " +   numController.text  ,
+                          'user_message2': "\n\nالسنه : " + yearcontroller.text + "\n\n" ,
+                          'user_message3': "\n\nالمؤمن له : " + incontroller.text + "\n\n" ,
+                          'user_message4':  "\n\nنوع التأمين : " + dropdownValue,
+                        }
+                      },
+                    )
+                );
+                print(response.body);
+
+
+
+
+                /* final Email send_email = Email(
     body: "رقم العقد او الملحق : " + numController.text + "\n" +
     "السنه : " + yearcontroller.text + "\n" +
     "المؤمن له : " + incontroller.text + "\n" +
@@ -154,17 +167,16 @@ class _aksState extends State<aks> {
     recipients: ['219SE2179@isik.edu.tr'],
     isHTML: true,
     );
-
     await FlutterEmailSender.send(send_email);
 */
-    }, child: Text("Continue"))
-    ],
+              }, child: Text("Continue"))
+            ],
 
 
-    ),
+          ),
 
-    ),
-    );
+        ),
+      );
 
 
 }
